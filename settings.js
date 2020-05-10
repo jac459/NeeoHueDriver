5f2f6d06d9898ec79f5886d0263c8ef654ab3368
jac459 <59643376+jac459@users.noreply.github.com>
2020-05-10 14:06:10 +0800

    Update settings with resources path

--- settings.js
+++ settings.js
@@ -6,7 +6,8 @@ var settings =
 "neeobrainip": "192.168.1.26",
 "neeobrainport": "5001",
 "philipshuebridgeuser": "",
-"imagesurl": "http://192.168.1.11:3000/store/"
+//"imagesurl": "http://192.168.1.11:3000/store/"
+"imagesurl": "https://raw.githubusercontent.com/jac459/NeeoHueDriver/master/Resources/"
 }
   
 module.exports = settings;
